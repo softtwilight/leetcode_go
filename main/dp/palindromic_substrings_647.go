f8b26efe54d5c4379417fd0d7e4835e7db80c47b
softtwilight <tjuyxl@gmail.com>
2021-01-24 23:10:41 +0900

    a more efficient way

--- main/dp/palindromic_substrings_647.go
+++ main/dp/palindromic_substrings_647.go
@@ -33,6 +33,26 @@ func isPalindromic(s string) bool {
 	return true
 }
 
+
+// a more efficient version, expand from mid
+// the change the mid index
+// from one index or two
+func countSubstrings2(s string) int {
+	sl := len(s)
+	count := 0
+	for mid := 0; mid < 2 * sl - 1; mid++ {
+		lo := mid / 2
+		hi := lo + mid % 2
+		for lo >= 0 && hi < sl && s[lo] == s[hi] {
+			count++
+			lo--
+			hi++
+		}
+	}
+	return count
+}
+
 func main() {
-	fmt.Println(countSubstrings("aaa"))
+	fmt.Println(countSubstrings("abccb"))
+	fmt.Println(countSubstrings2("abccb"))
 }
