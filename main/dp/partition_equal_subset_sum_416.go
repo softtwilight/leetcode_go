b8d4ad5cbda9af40c07a39ba8f574eb9d5e0bc0b
softtwilight <tjuyxl@gmail.com>
2021-01-28 00:24:21 +0900

    compile error

--- main/dp/partition_equal_subset_sum_416.go
+++ main/dp/partition_equal_subset_sum_416.go
@@ -91,5 +91,5 @@ func canPartition2(nums []int) bool {
 
 func main() {
 	fmt.Println(canPartition([]int{1,2,3,4,5,6,7}))
-	fmt.Println(canPartition3([]int{1,2,3,4,5,6,7}))
+	//fmt.Println(canPartition3([]int{1,2,3,4,5,6,7}))
 }
