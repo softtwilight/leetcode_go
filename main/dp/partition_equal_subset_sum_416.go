5671866af0200716d6b2db848c4fef38898f5e22
softtwilight <tjuyxl@gmail.com>
2021-01-27 23:29:04 +0900

    new version

--- main/dp/partition_equal_subset_sum_416.go
+++ main/dp/partition_equal_subset_sum_416.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 //https://leetcode.com/problems/partition-equal-subset-sum/
@@ -11,41 +10,48 @@ func canPartition(nums []int) bool {
 	for _, v := range nums {
 		sum += v
 	}
-	if sum%2 != 0 {
+	if sum % 2 != 0 {
 		return false
 	}
-
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] < nums[j]
-	})
-
-	memo := make(map[int]int)
-	return helper1(sum/2, nums, len(nums)-1, memo)
-
-}
-
-func helper1(target int, nums []int, hi int, memo map[int]int) bool {
-	if target == 0 {
-		return true
-	}
-	if target < 0 || hi < 0 {
-		return false
-	}
-	if v, ok := memo[target]; ok {
-		if nums[v] == nums[hi] {
-			return false
+	sum = sum / 2
+	memo := make([]bool, sum + 1)
+	memo[0] = true
+	for i := 0; i < len(nums); i++ {
+		for j := sum; j >= nums[i]; j-- {
+			memo[j] = memo[j] || memo[j - nums[i]]
 		}
 	}
-	for i := hi; i >= 0; i-- {
-		if nums[hi] <= target {
-			if helper1(target-nums[hi], nums, i-1, memo) {
-				return true
-			}
-		}
-	}
-	memo[target] = hi
-	return false
+	return memo[sum]
 }
+
+//
+//	memo := make(map[int]int)
+//	return helper1(sum/2, nums, len(nums)-1, memo)
+//
+//}
+
+//func helper1(target int, nums []int, hi int, memo map[int]int) bool {
+//	if target == 0 {
+//		return true
+//	}
+//	if target < 0 || hi < 0 {
+//		return false
+//	}
+//	if v, ok := memo[target]; ok {
+//		if nums[v] == nums[hi] {
+//			return false
+//		}
+//	}
+//	for i := hi; i >= 0; i-- {
+//		if nums[hi] <= target {
+//			if helper1(target-nums[hi], nums, i-1, memo) {
+//				return true
+//			}
+//		}
+//	}
+//	memo[target] = hi
+//	return false
+//}
 
 func main() {
 	fmt.Println(canPartition([]int{2, 2, 1, 1}))
